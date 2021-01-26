ce30201e69abe8591f85cb5d64681504ff8f20a2
Bogdan-Cristian Ciumac <bogdanciumac200@gmail.com>
2021-01-26 10:57:03 +0100

    added Session controll class and installed outer for navigation

--- index.php
+++ index.php
@@ -1,3 +1,19 @@
 <?php
 
+require "libs/session_control.php";
+session_start();
+$session = new \libs\session_control();
 
+
+require_once __DIR__ . '/vendor/autoload.php';
+
+// Use this namespace
+use Steampixel\Route;
+
+// Add your first route
+Route::add('/html', function() {
+    include("login.php");
+});
+
+// Run the router
+Route::run('/');
